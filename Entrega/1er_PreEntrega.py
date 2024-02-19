1c9b96ad07f6054e3767940fce82eb950143abc4
Joaquin Gonzalez Zago <jgzago89@gmail.com>
2024-02-19 12:09:27 -0300

    Version 1.0

---
+++ Entrega/1er_PreEntrega.py
@@ -0,0 +1,60 @@
+"""
+El sistema consta de 3 funciones ('registro', 'mostrar' y 'login') que se encargan de realizar las operaciones requeridas por el enunciado del trabajo
+Por cuestiones de legibilidad y consigna, estas funciones estan contenidas en otra funcion principal llamada 'main'
+Solamente hay que llamar a la funcion 'main' y esta se encarga de la simulacion completa ya que llama a las funciones dependiendo de lo que el usuario desee
+Todas las funciones poseen como parametro una variable que tiene que ser del tipo diccionario para que funcione correctamente
+
+"""
+def registro(bdd):
+  nuevo_archivo = open("BaseDeDatos.txt", "w")
+  flag = True
+  if type(bdd) != dict:
+    flag = False
+    print("Ingreso un parametro incorrecto. Ingrese un diccionario valido")
+  while flag == True:
+    try:
+      usuario = input("Ingrese Usuario: ")
+      password = input("Ingrese Contraseña: ")
+      bdd[usuario] = password
+      nuevo_archivo.write(str(usuario) + str(" ") + str(password) + "\n" )
+      flag = bool(input("Si desea continuar ingrese un valor, de lo contrario deje la casilla en blanco: "))
+    except Exception as e:
+      print(f"Se produjo un error {type(e).__name__}")
+
+def mostrar(bdd):
+  if type(bdd) != dict:
+    print("Ingreso un parametro incorrecto. Ingrese un diccionario valido")
+  else:
+    if not bdd:
+      print("La base de datos está vacía.")
+    else:
+      print("La base de datos seleccionada contiene la siguiente informacion")
+      print(bdd)
+
+def login(bdd):
+  user = input("Ingrese usuario a buscar: ")
+  cont = input("Ingrese su contraseña: ")
+  if user in bdd and bdd[user] == cont:
+    print("La clave y el valor COINCIDEN con una entrada en el diccionario.")
+  else:
+    print("La clave y el valor NO COINCIDEN con ninguna entrada en el diccionario.")
+
+def main(bdd):
+  print("Bienvenido al sistema gestor de bases de datos")
+  opcion = input(" Ingrese '1' para registrar nuevos datos \n Ingrese '2' para visualizar los datos \n Ingrese '3' para realizar un login \n Ingrese '4' para salir: \n Su opcion: " )
+  flag = True
+  while flag == True:
+    if opcion == '1':
+      registro(bdd)
+      opcion = input(" Ingrese '1' para registrar nuevos datos \n Ingrese '2' para visualizar los datos \n Ingrese '3' para realizar un login \n Ingrese '4' para salir: \n Su opcion: " )
+    elif opcion == '2':
+      mostrar(bdd)
+      opcion = input(" Ingrese '1' para registrar nuevos datos \n Ingrese '2' para visualizar los datos \n Ingrese '3' para realizar un login \n Ingrese '4' para salir: \n Su opcion: " )
+    elif opcion =='3':
+      login(bdd)
+      opcion = input(" Ingrese '1' para registrar nuevos datos \n Ingrese '2' para visualizar los datos \n Ingrese '3' para realizar un login \n Ingrese '4' para salir: \n Su opcion: " )
+    elif opcion =='4':
+      flag = False
+    else:
+      print("Ocurrio un error, ingrese su opcion nuevamente")
+      opcion = input(" Ingrese '1' para registrar nuevos datos \n Ingrese '2' para visualizar los datos \n Ingrese '3' para realizar un login \n Ingrese '4' para salir: \n Su opcion: " )
---
+++ Entrega/2da_PreEntrega.py
@@ -0,0 +1,92 @@
+class Cliente: #Defino clase cliente especificando atributos y metodos, cabe destacar que el carrito es un atributo 
+    def __init__(self,nombre,apellido,carrito,edad,vip) -> None:
+        self.nombre = nombre
+        self.apellido = apellido
+        self.carrito = carrito
+        self.edad = edad
+        self.vip = vip #Este campo indica con un booleano si el cliente es VIP o no
+    
+    def __str__(self):
+        return f"Cliente: {self.nombre} {self.apellido}"    
+
+    def addcarrito(self, articulo):
+        return self.carrito.agregar_articulo(articulo)
+
+    def elimcarrito(self, articulo):
+        return self.carrito.eliminar_articulo(articulo)
+
+    def calcular_total(self):
+        return self.carrito.calculartotal()
+
+class ClienteVIP(Cliente): #Clase que deriva de cliente. Los clientes VIP disfrutan de descuentos exclusivos 
+    def __init__(self, nombre, apellido, carrito, edad, vip, descuento) -> None:
+        super().__init__(nombre, apellido, carrito, edad, vip)
+        self.descuento = descuento #los descuentos se expresan en decimales ej 0.5 = 50% off
+    
+    def calcular_total(self):
+        total = super().calcular_total()
+        if self.vip:
+            print(f"Total con descuento: ${total*self.descuento}")
+            return total*self.descuento
+        else:
+            print(f"Total del carrito: ${total}")
+            return total
+
+class Carrito:
+    def __init__(self):
+        self.articulos = []
+
+    def agregar_articulo(self, articulo):
+        self.articulos.append(articulo)
+        print(f"{articulo} agregado al carrito con exito.")
+
+    def eliminar_articulo(self, articulo):
+        if articulo in self.articulos:
+            self.articulos.remove(articulo)
+            print(f"{articulo} eliminado del carrito con exito.")
+        else:
+            print(f"{articulo} no encontrado en el carrito.")
+
+    def calculartotal(self):
+        total = [articulo.precio for articulo in self.articulos]
+        print(f"Total del carrito: ${total}")
+        return sum(total)
+    
+    def __str__(self):
+        if not self.articulos:
+            return "Carrito vacío"
+        else:
+            return "\n".join(str(articulo) for articulo in self.articulos)
+
+
+class Articulo:
+    def __init__(self, nombre, precio) -> None:
+        self.nombre = nombre
+        self.precio = precio
+    
+    def __str__(self) -> str:
+        return (f"Articulo: {self.nombre} precio: {self.precio}")
+
+
+item1 = Articulo("Manzana", 100)
+item2 = Articulo("Banana", 150)
+car = Carrito()
+car.agregar_articulo(item1)
+car.agregar_articulo(item2)
+print(car)
+
+
+""" 
+car = Carrito()
+carvip = Carrito()
+item1 = Articulo("Manzana", 100)
+item2 = Articulo("Banana", 150)
+clienteNormal = Cliente("Juan", "Perez", car, 22, False)
+clientevp = ClienteVIP("Bruno", "Zago", carvip, 70, True, 0.5)
+
+clienteNormal.addcarrito(item1)
+clientevp.addcarrito(item1)
+
+clienteNormal.calcular_total()
+clientevp.calcular_total()
+"""
